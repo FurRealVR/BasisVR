29eb70008e0691f4a413f3b2481e257a9a36e98b
OfficialR3ido101 <39566484+OfficialR3ido101@users.noreply.github.com>
2025-01-19 00:07:53 +0000

    Made basic case switch for commands.

--- Basis Server/BasisNetworkServer/Websocket/CommandHandler.cs
+++ Basis Server/BasisNetworkServer/Websocket/CommandHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Basis.Network.Server.SocketChat
@@ -10,7 +11,17 @@ namespace Basis.Network.Server.SocketChat
     {
         public static async Task processCommandAsync(WebSocket ws, String command)
         {
-            await ws.SendAsync("Not implemented.");
+            switch (command.ToLower()) {
+                case "ping":
+                    var ping = Encoding.UTF8.GetBytes("PONG!");
+                    await ws.SendAsync(new ArraySegment<byte>(ping), WebSocketMessageType.Text, true, CancellationToken.None);
+                    break;
+                default:
+                    var defaultResponse = Encoding.UTF8.GetBytes("Not Implemented.");
+                    await ws.SendAsync(new ArraySegment<byte>(defaultResponse), WebSocketMessageType.Text, true, CancellationToken.None);
+                    break;
+            }
+            
         }
     }
 }
